ba24da652ea7c0f14ad986612c59e81022916888
dgavieira <dialcantaravieira@hotmail.com>
2015-08-25 17:34:56 -0400

    Telas
    Telas de login e cadastro desenhadas em XML

--- AmbulanciaJa/app/src/main/java/com/example/usuario/ambulanciaja/tela_socorro.java
+++ AmbulanciaJa/app/src/main/java/com/example/usuario/ambulanciaja/tela_socorro.java
@@ -6,18 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TelaCadastro extends ActionBarActivity {
+public class tela_socorro extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_cadastro);
+        setContentView(R.layout.activity_tela_socorro);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tela_cadastro, menu);
+        getMenuInflater().inflate(R.menu.menu_tela_socorro, menu);
         return true;
     }
 
